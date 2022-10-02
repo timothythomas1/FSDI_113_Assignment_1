8887827030d1481095199990815cf6d8945e1996
timothythomas1 <tuxes-parlay0s@icloud.com>
2022-10-01 19:57:46 -0700

    Added registration folder with logged_out, login HTMLS

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('posts/', include('posts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
